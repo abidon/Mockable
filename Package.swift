588943dc0fe8c7d9fe1d2f6cbe239da6c8e4f3a2
Aurelio <abidon@users.noreply.github.com>
2024-08-16 18:44:58 +0200

    Point swift-syntax dependency to swiftlang/swift-syntax

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
         ),
     ],
     dependencies: ifDev(add: devDependencies) + [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
         .package(url: "https://github.com/abidon/OpenCombine.git", branch: "feature/combine-latest"),
     ],
     targets: ifDev(add: devTargets) + [
